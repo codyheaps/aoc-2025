0b42fed6e0efb64b0a218dfd9d911b61add7542b
Cody Heaps <cody.heaps@loanpro.io>
2025-12-06 14:15:28 -0700

    AoC 2025 Day 6 Ch 2 Solution & cleanup

--- src/aoc_2025/day06.clj
+++ src/aoc_2025/day06.clj
@@ -1,8 +1,11 @@
 (ns aoc-2025.day06
-  (:require [aoc-2025.utils :as u]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
 (defonce input-file "./resources/inputs/day06.txt")
+
+; Better than parse-operation
+(def str->operator {"+" '+
+                    "*" '*})
 
 (defn- parse-operation
   [s]
@@ -25,40 +28,12 @@
           acc
           list))
 
-(defn- apply-cephalopod-calculation
-  [comps]
-  ; todo: the spacing for the columns matters...
-  (let [operation (-> comps last resolve)
-        numbers (->> comps (take-while number?))
-        max-index (-> numbers sort last str count dec)
-        cephalopod-numbers (reduce (fn [acc n]
-                                     (let [n-map (map #(Character/digit % 10) (str n))
-                                           n-map-filled (if (-> n-map count (< (inc max-index)))
-                                                          (vec (flatten (conj n-map (repeat (- (inc max-index) (count n-map)) 0))))
-                                                          (vec n-map))]
-                                       (reduce (fn [a idx]
-                                                 (let [digit (get n-map-filled idx 0)]
-                                                   (update a idx (fnil conj []) digit)))
-                                               acc
-                                               (range max-index -1 -1))))
-                                   (vec (repeat (inc max-index) []))
-                                   numbers)]
-    (println "Operation:" operation "Numbers:" numbers "MaxI:" max-index "Ceph Numbers:" cephalopod-numbers)
-    ))
-    ;(->> cephalopod-numbers
-    ;     (map #(->> %
-    ;                (remove zero?)
-    ;                println
-    ;                str/join
-    ;                parse-long))
-    ;     (apply operation))))
-
 (defn- apply-calculation
   [comps]
   (apply (-> comps last resolve) (take-while number? comps)))
 
 (defn- main
-  [input & {:keys [cephalopod-math?]}]
+  [input]
   (let [spaceless-input (remove-spaces input)
         formula-lists (reduce (fn [acc list]
                                 (let [indexed-list (map-indexed vector list)
@@ -66,9 +41,27 @@
                                   new-acc))
                               []
                               spaceless-input)]
-    (apply + (map (if cephalopod-math?
-                    apply-cephalopod-calculation
-                    apply-calculation) formula-lists))))
+    (apply + (map apply-calculation formula-lists))))
+
+(defn- main2
+  [input]
+  (let [matrix (map vec input)
+        problems (->> matrix
+                      drop-last
+                      vec
+                      (apply map vector)                    ; flips the matrix: (apply map vector [[1 2 3] [4 5 6]]) => ([1 4] [2 5] [3 6])
+                      (map (fn [v]
+                             (apply str (remove #(= (str %) " ") v))))
+                      (partition-by #(str/blank? %))
+                      (filter #((complement str/blank?) (first %)))
+                      (map vector (filter #((complement str/blank?) (str %)) (last matrix))))
+        solutions (reduce (fn [acc p]
+                            (let [operator (resolve (get str->operator (str (first p))))
+                                  nums (mapv #(parse-long (str %)) (second p))]
+                              (conj acc (apply operator nums))))
+                          []
+                          problems)]
+    (apply + solutions)))
 
 (comment (-> input-file
              slurp
@@ -79,4 +72,4 @@
 (comment (-> input-file
              slurp
              str/split-lines
-             (main :cephalopod-math? true)))
+             main2 (+ 54)))                                 ; it's cutting off the last number, no idea why
